5c1edaff2c87b132b0041cbb7d21002d3f85ebcd
KASINGYE <michaelkasingye@gmail.com>
2020-04-24 03:27:07 +0300

    Added slide drawer menu folder and contents

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
-
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 const layout = (props) => (
     <Auxiliary>
     <Toolbar/>
-    
+    <SideDrawer/>
     <main className={classes.content}>
         {props.children}
     </main>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,6 +2,8 @@ import React from 'react';
 import burgerLogo from '../../assests/images/burger-logo.png';
 import classes from './Logo.css';
 
+
+
 const logo = (props) =>(
     <div className={classes.Logo}>
         <img src={burgerLogo} alt="MyBurger"/>
